1f06bde01e7c6b68e316d71318a267f3dd344ff0
simneol <simneol09784@gmail.com>
2016-04-12 13:40:05 +0900

    push&amp;pull send stdout to channel

--- apps/system.py
+++ apps/system.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from decorators import on_command
 import subprocess
+from subprocess import check_output
 @on_command(['SYSTEM'])
 def run(robot, channel, tokens, user):
     ''' 일반 사용자는 이용할 수 없습니다'''
@@ -14,13 +15,14 @@ def run(robot, channel, tokens, user):
         subprocess.call(['/home/simneol/hongmoa/reboot.sh'])
         return channel, 'Rebooting...'
     elif str(tokens[0]) == 'PULL':
-        subprocess.call(['/home/simneol/hongmoa/git_pull.sh'])
-        return channel, 'Pulled from github'
+        #subprocess.call(['/home/simneol/hongmoa/git_pull.sh'])
+        return channel, check_output(['/home/simneol/hongmoa/git_pull.sh'])
     elif str(tokens[0]) == 'PUSH':
         arg = ''
         for token in tokens:
             arg+=str(token)+' '
         arg = arg[4:-1]
-        subprocess.call(['/home/simneol/hongmoa/git_push.sh',arg])
-        return channel, 'Pushed to github'
+        #subprocess.call(['/home/simneol/hongmoa/git_push.sh',arg])
+        return channel, check_output(['/home/simneol/hongmoa/git_push.sh',arg])
+
     return channel, '...'
